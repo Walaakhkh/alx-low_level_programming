0303060a88955f4d89a20ac9cb0e9a2efde91bf5
@Walaakhkh <Walaakhalil98@gmail.com>
2023-09-13 12:19:45 -0700

    main function

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -2,11 +2,11 @@
 
 /**
  * main - check the code
- * print_alphabet - print alphabets in lower case
+ *
  * Return: Always 0.
  */
 int main(void)
 {
-   	print_alphabet();
-    return (0);
+   print_alphabet();
+return (0);
 }
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -3,12 +3,21 @@
 
 /*
  * File: main.h
- * desc: Header file contains declarations used in 0x02-functions_nested_loops 
+ * Desc: Header file containing declarations used in 0x02-functions_nested_loops 
  */
 
 int _putchar(char c);
 void print-alphabet(void);
 void print-alphabet_x10(void);
 int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
 
 #endif
