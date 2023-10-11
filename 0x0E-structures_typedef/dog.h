01752cab6a84365245061c88620d7c32653c30f8
@Walaakhkh <Walaakhalil98@gmail.com>
2023-10-11 12:52:39 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,8 @@
+#ifndef DOG_H
+#define DOG_H
+
+void struct dog(char *name, float age, char *owner);
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif
