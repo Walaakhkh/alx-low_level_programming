b813d53e9352e157886e0e7925b231a63c65145a
@Walaakhkh <Walaakhalil98@gmail.com>
2023-10-12 06:57:00 -0700

    task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,7 +17,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 	if (separator != NULL)
 	{
-	printf("%d", va_arg(arg, int));	
+	printf("%d", va_arg(arg, int));
+	if (i != n -1)
 	printf("%s", separator);
 	}
 	else
