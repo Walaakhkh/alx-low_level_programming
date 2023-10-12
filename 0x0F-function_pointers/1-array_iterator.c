8e4613f1076acab3751949efed6564c0d33319fe
@Walaakhkh <Walaakhalil98@gmail.com>
2023-10-12 01:23:14 -0700

    task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include "function_pointers.h"
 #include <stdio.h>
 /**
@@ -10,7 +11,7 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i;
+	unsigned int i;
 
 	if (array == NULL || action == NULL)
 		return;
