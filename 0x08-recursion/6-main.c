8dcf500c0488c898c56e51067d4d6fae918a1c7e
Walaakhkh <walaakhalil98@gmail.com>
2023-11-19 00:20:55 -0800

    task 6

--- 0x08-recursion/6-main.c
+++ 0x08-recursion/6-main.c
@@ -22,7 +22,7 @@ int main(void)
 	printf("%d\n", r);
 	r = is_prime_number(-1);
 	printf("%d\n", r);
- 	 = is_prime_number(113);
+ 	r = is_prime_number(113);
 	printf("%d\n", r);
 	r = is_prime_number(7919);
 	printf("%d\n", r);
