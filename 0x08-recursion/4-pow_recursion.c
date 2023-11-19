aad44ca17b330e752a2750f936509a0869cc9fd6
Walaakhkh <walaakhalil98@gmail.com>
2023-11-18 23:15:27 -0800

    task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,3 +1,19 @@
 #include "main.h"
 
+/**
+ * _pow_recursion - find a power of a number over number
+ * @x: base number
+ * @y: power
+ * Return: x power y
+ */
+
 int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, (y - 1)));
+	return (0);
+}
