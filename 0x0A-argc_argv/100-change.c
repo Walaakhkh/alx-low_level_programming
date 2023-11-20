fb0c8c33de155aac2799209748795fa5922cbd50
Walaakhkh <walaakhalil98@gmail.com>
2023-11-20 10:53:53 -0800

    task 100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -16,28 +16,28 @@ int main(int argc, char *argv[])
 
 	if (argc != 2)
 	{
-		printf("Error\n");
-		return (1);
+	printf("Error\n");
+	return (1);
 	}
 
 	x = atoi(argv[1]);
 
 	if (x < 0)
-		printf("%d\n", 0);
+	printf("%d\n", 0);
 
 	while (x > 0)
 	{
-		if (x >= 25)
-			x = x - 25;
-		else if (x >= 10)
-			x = x - 10;
-		else if (x >= 5)
-			x = x - 5;
-		else if (x >= 2)
-			x = x - 2;
-		else if (x >= 1)
-			x = x - 1;
-		y++;
+	if (x >= 25)
+	x = x - 25;
+	else if (x >= 10)
+	x = x - 10;
+	else if (x >= 5)
+	x = x - 5;
+	else if (x >= 2)
+	x = x - 2;
+	else if (x >= 1)
+	x = x - 1;
+	y++;
 	}
 	printf("%d\n", y);
 	return (0);
