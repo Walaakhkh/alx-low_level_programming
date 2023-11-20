7bbe89828177a1c7b201026b320c381fff9c43c2
Walaakhkh <walaakhalil98@gmail.com>
2023-11-20 08:09:59 -0800

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -18,11 +18,11 @@ int main(int argc, char *argv[])
 		printf("%d\n", 0);
 	else
 	{
-		for (i = 0 ; i < argc ; i++)
+		for (i = 1 ; i < argc ; i++)
 		{
 			for (j = 0 ; argv[i][j] != '\0' ; j++)
 			{
-			if (!(isdigit(argv[i][j])))
+				if (!(isdigit(argv[i][j])))
 				{
 					printf("Error\n");
 					return (1);
