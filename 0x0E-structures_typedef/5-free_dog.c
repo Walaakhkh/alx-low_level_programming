47562c942fb96facb52ded9706f21035b544594a
Walaakhkh <walaakhalil98@gmail.com>
2023-11-27 10:01:41 -0800

    task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * free_dog - function that frees dogs
+ * @d: dog to free
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
