1a3f0b866aea41abeda48ac68d615e95bb64e10e
Walaakhkh <walaakhalil98@gmail.com>
2023-11-29 03:46:30 -0800

    task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,11 @@
-#include "function_pointers.h"
 #include <stdio.h>
+#include "function_pointers.h"
 
- /**
-  * print_name - function that prints name
-  * @name: name to be printed
-  * @f: function pointer
-  */
+/**
+ * print_name - function that prints name
+ * @name: name to be printed
+ * @f: function pointer
+ */
 
 void print_name(char *name, void (*f)(char *))
 {
