3c258ca6c9562c42b8951e6eda4402836a21fad3
Walaaakhkh <Walaakhalil98@gmail.com>
2024-07-17 13:39:30 -0700

    task 13

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,7 +1,29 @@
+#include <stdlib.h>
 #include <stddef.h>
 #include <stdio.h>
+
 #ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
+
+/**
+ * struct skiplist_s - Singly linked list with express lane
+ *
+ * @n: Integer stored in the node
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node in the list
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ * for Holberton project
+ */
+typedef struct skiplist_s
+{
+    int n;
+    size_t index;
+    struct skiplist_s *next;
+    struct skiplist_s *express;
+} skiplist_t;
+skiplist_t *linear_skip(skiplist_t *list, int value);
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
@@ -9,6 +31,5 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
-
 
 #endif
