6d0df4bf421421f30dc023b08a84ac4362d5cd7e
Walaaakhkh <Walaakhalil98@gmail.com>
2024-07-18 08:10:04 -0700

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -9,16 +9,3 @@ def safe_print_list(my_list=[], x=0):
         pass
     print('')
     return count
-
-# Testing the function as per your example
-
-my_list = [1, 2, 3, 4, 5]
-
-nb_print = safe_print_list(my_list, 2)
-print("nb_print: {:d}".format(nb_print))
-
-nb_print = safe_print_list(my_list, 5)
-print("nb_print: {:d}".format(nb_print))
-
-nb_print = safe_print_list(my_list, 7)
-print("nb_print: {:d}".format(nb_print))
