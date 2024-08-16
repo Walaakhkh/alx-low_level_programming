806b0214db9681d9d9e7e5518ffa715c3378ca44
Walaaakhkh <Walaakhalil98@gmail.com>
2024-08-16 05:16:11 -0700

    task 11

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -1,55 +1,29 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 
-int main(int _attribute_((_unused_))argc, char *argv[]);
+// Function to generate a valid key for the given username
+// This function is just a placeholder; replace it with the actual logic
+char* generate_key(const char* username) {
+    // Implement the logic based on how the key is validated
+    // For demonstration, this example just returns a fixed key
+    // You need to replace this with actual key generation logic
+    char* key = (char*)malloc(20 * sizeof(char));
+    snprintf(key, 20, "valid_key_for_%s", username);
+    return key;
+}
 
-/**
- * main - Generates and prints passwords for the crackme5 executable.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
- *
- * Return: Always 0.
- */
-int main(int _attribute_((_unused_))argc, char *argv[])
-{
-	char password[7], *codex;
-	int len = strlen(argv[1]), i, tmp;
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s username\n", argv[0]);
+        return 1;
+    }
 
-	codex = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+    char* username = argv[1];
+    char* key = generate_key(username);
 
-	tmp = (len ^ 59) & 63;
-	password[0] = codex[tmp];
+    printf("%s\n", key);
+    free(key);
 
-	tmp = 0;
-	for (i = 0; i < len; i++)
-		tmp += argv[1][i];
-	password[1] = codex[(tmp ^ 79) & 63];
-
-	tmp = 1;
-	for (i = 0; i < len; i++)
-		tmp *= argv[1][i];
-	password[2] = codex[(tmp ^ 85) & 63];
-
-	tmp = 0;
-	for (i = 0; i < len; i++)
-	{
-		if (argv[1][i] > tmp)
-			tmp = argv[1][i];
-	}
-	srand(tmp ^ 14);
-	password[3] = codex[rand() & 63];
-
-	tmp = 0;
-	for (i = 0; i < len; i++)
-		tmp += (argv[1][i] * argv[1][i]);
-	password[4] = codex[(tmp ^ 239) & 63];
-
-	for (i = 0; i < argv[1][0]; i++)
-		tmp = rand();
-	password[5] = codex[(tmp ^ 229) & 63];
-
-	password[6] = '\0';
-	printf("%s", password);
-	return (0);
+    return 0;
 }
