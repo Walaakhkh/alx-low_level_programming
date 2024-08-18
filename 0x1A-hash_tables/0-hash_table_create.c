1da13084a7d718c0503cde4a497288645f358b09
Walaaakhkh <Walaakhalil98@gmail.com>
2024-08-18 07:00:58 -0700

    task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -18,9 +18,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (new_table == NULL)
 	return (NULL);
 
-    new_table->array = malloc(sizeof(hash_node_t *) * size);
-    if (new_table->array == NULL)
-    {
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (new_table->array == NULL)
+	{
 	free(new_table);
 	return (NULL);
 	}
